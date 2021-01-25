0258035fa5c1fbedced89fd065f5d970c7e3b3aa
sallymatos <sally.matos322@gmail.com>
2021-01-24 21:51:24 -0600

    Homework - Intro to Java

---
+++ src/com/tts/Main.java
@@ -0,0 +1,15 @@
+package com.tts;
+
+public class Main {
+    String main;
+    public static void main(String[] args) {
+
+
+       // System.out.println(Main.main);
+        // Line above is broken
+        System.out.println("hi");
+        String main2 = "bananas";
+        System.out.println(main2);
+    }
+
+}
